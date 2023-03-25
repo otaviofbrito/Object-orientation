24189364701c8e6e25643afab25bd2bf392c46c3
Otavio Brito <otaviofbrito@gmail.com>
2023-03-25 19:08:05 -0300

    aula2

--- aula2/Hello World/src/oo/intellij/Main.java
+++ aula2/Hello World/src/oo/intellij/Main.java
@@ -3,6 +3,11 @@ package oo.intellij;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        if(args.length != 0){
+            for(int i  = 0; i< args.length; i++){
+                System.out.println(args[i]);
+            }
+        }
+        System.out.println("FIM");
     }
 }
