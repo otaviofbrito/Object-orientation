404835cebd549f768971fee6149c0ff416216066
Otavio Brito <otaviofbrito@gmail.com>
2023-04-26 21:58:10 -0300

    constructor

---
+++ aula4-maven/aula4-mvn/src/test/java/me/otaviobrito/AppTest.java
@@ -0,0 +1,20 @@
+package me.otaviobrito;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
