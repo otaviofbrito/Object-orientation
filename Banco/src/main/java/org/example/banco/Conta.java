041ef92ed34f8995b0b2caeb67b8e2feccc9b1ac
Otavio Brito <otaviofbrito@gmail.com>
2023-05-20 17:33:02 -0300

    banco01

--- Banco/src/main/java/org/example/banco/Conta.java
+++ Banco/src/main/java/org/example/banco/Conta.java
@@ -1,4 +1,23 @@
 package org.example.banco;
 
 public class Conta {
+    private double saldo;
+
+    public Conta(double saldoInicial){
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public void depositar(double valor){
+        this.saldo+=valor;
+    }
+    public void sacar(double valor){
+        if(valor <= saldo){
+            saldo -= valor;
+        }
+    }
+
 }
--- Banco/src/main/java/org/example/banco/TesteBanco.java
+++ Banco/src/main/java/org/example/banco/TesteBanco.java
@@ -4,6 +4,15 @@ public class TesteBanco {
 
     public static void main(String[] args){
 
+        Conta cliente = new Conta(1000);
+        System.out.println("Criando conta com saldo de: " + cliente.getSaldo());
+        System.out.println("sacando: 250");
+        cliente.sacar(250);
+        System.out.println("depositando: 320.50");
+        cliente.depositar(320.50);
+        System.out.println("sacando: 120");
+        cliente.sacar(120);
+        System.out.println("saldo de: " + cliente.getSaldo());
     }
 
 }
